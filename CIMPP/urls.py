bac991ab649b81e907ed737e8e78ecbfaf109408
Lankoande Enock <lankoandeenock002@gmail.com>
2026-08-01 14:19:55 +0000

    Refactor URL imports and comment out unused media serving code

--- CIMPP/urls.py
+++ CIMPP/urls.py
@@ -15,12 +15,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import include, path, re_path
+from django.urls import include, path #re_path
 from django.conf import settings
 from django.conf.urls.static import static
-from api.views import serve_media 
+# from api.views import serve_media 
 
-from api import urls
+# from api import urls
 
 
 urlpatterns = [
@@ -30,10 +30,10 @@ urlpatterns = [
     path("api-auth/", include("rest_framework.urls")),
 ]
 
-if getattr(settings, 'USE_GARAGE', False):
-    urlpatterns += [
-        re_path(r'^media/(?P<path>.+)$', serve_media, name='serve_media'),
-    ]
+# if getattr(settings, 'USE_GARAGE', False):
+#     urlpatterns += [
+#         re_path(r'^media/(?P<path>.+)$', serve_media, name='serve_media'),
+#     ]
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
